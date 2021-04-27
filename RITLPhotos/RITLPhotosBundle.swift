f3b51bc4ce081b44bfbccadb428169d1950691b9
岳文 <yuexiaowen108@gmail.com>
2021-04-27 16:17:23 +0800

    update 更新bundle图片

--- RITLPhotos/RITLPhotosBundle.swift
+++ RITLPhotos/RITLPhotosBundle.swift
@@ -11,42 +11,52 @@ import UIKit
 /// RITLPhotos 使用的图片
 enum RITLPhotosImage: String {
     
+    //全局
+    
+    case photos_blur = "ritl_blur.png"
+    
     /// 导航栏关闭的按钮
     case nav_close = "ritl_photos_nav_close.png"
+    /// 浏览器返回的按钮
+    case nav_back = "ritl_photos_nav_back.png"
+    /// 浏览右上角的选中
+    case nav_deselect = "ritl_photos_nav_deselected.png"
+    
     
     /// 下方bar中原图按钮的选中
     case tool_bottom_selecte = "ritl_photos_bottom_selected.png"
     /// 下方bar中原图按钮的默认未选中
     case tool_bottom_deselect = "ritl_photos_bottom_deselected.png"
     
+    
     /// 顶部导航的箭头按钮
     case pick_top_arrow = "ritl_photos_picker.png"
+    
+    
+    /// 占位图
+//    case placeholder = "ritl_placeholder.png"
+    /// 右侧的箭头
+//    case arrowRight = "ritl_arrow_right.png"
+    
+    //相册组
     
     /// 分组选中的标记
     case group_select =  "ritl_photos_group_select.png"
     
     
-    /// 占位图
-    case placeholder = "ritl_placeholder.png"
-    /// 右侧的箭头
-    case arrowRight = "ritl_arrow_right.png"
-    
     //集合视图
     
-    /// 集合视图
     case collection_normal = "ritl_photos_deselect.png"
     /// 集合视图中的视频小icon
-    case collection_video_icon = "ritl_photos_m_video.png"
+//    case collection_video_icon = "ritl_photos_m_video.png"
     
     //浏览视图
     
-    /// 浏览右上角的选中
-    case browerSelect = "ritl_brower_selected.png"
-    /// 浏览左上角的返回
-    case browseBack = "ritl_browse_back.png"
+
+
 
     /// 视频播放
-    case videoPlayer = "ritl_video_play.png"
+//    case videoPlayer = "ritl_video_play.png"
 }
 
 
