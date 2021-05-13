fbe0a3ed2071273bf2831e4d54c02b6b1a05efe3
岳文 <yuexiaowen108@gmail.com>
2021-05-13 14:29:51 +0800

    Update 支持Carthage

---
+++ RITLPhotos_swift/RITLPhotos_swift.h
@@ -0,0 +1,19 @@
+//
+//  RITLPhotos_swift.h
+//  RITLPhotos_swift
+//
+//  Created by YueWen on 2021/5/13.
+//  Copyright © 2021 YueWen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RITLPhotos_swift.
+FOUNDATION_EXPORT double RITLPhotos_swiftVersionNumber;
+
+//! Project version string for RITLPhotos_swift.
+FOUNDATION_EXPORT const unsigned char RITLPhotos_swiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RITLPhotos_swift/PublicHeader.h>
+
+
